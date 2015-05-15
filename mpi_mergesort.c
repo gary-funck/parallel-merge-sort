7837c36f57ce3af5143d23c7910a81139a2208cf
Gary Funck <gary@intrepid.com>
2015-05-15 10:36:25 -0700

    Remove comment re: temp needed for OpenMP.

--- mpi_mergesort.c
+++ mpi_mergesort.c
@@ -204,7 +204,6 @@ mergesort_serial (int a[], int size, int temp[])
     }
   mergesort_serial (a, size / 2, temp);
   mergesort_serial (a + size / 2, size - size / 2, temp);
-  // The above call will not work properly in an OpenMP program
   // Merge the two sorted subarrays into a temp array
   merge (a, size, temp);
 }
