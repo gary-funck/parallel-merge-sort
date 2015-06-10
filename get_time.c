4cd11348d2db36453df38416e040ce833a6c6416
Gary Funck <gary@intrepid.com>
2015-06-10 16:25:02 -0700

    Move get_time into a single source file.

---
+++ get_time.c
@@ -0,0 +1,68 @@
+/* get_time() function.
+   Copyright (C) 2015 Gary Funck <gary@intrepid.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2 of
+ the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public
+ License along with this program; if not, write to the Free
+ Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ Boston, MA  02110-1301, USA.
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#if _POSIX_TIMERS
+#include <time.h>
+#ifdef CLOCK_MONOTONIC_RAW
+/* System clock id passed to clock_gettime. CLOCK_MONOTONIC_RAW
+   is preferred.  It has been available in the Linux kernel
+   since version 2.6.28 */
+#define SYS_RT_CLOCK_ID CLOCK_MONOTONIC_RAW
+#else
+#define SYS_RT_CLOCK_ID CLOCK_MONOTONIC
+#endif
+
+double
+get_time (void)
+{
+  struct timespec ts;
+  double t;
+  if (clock_gettime (SYS_RT_CLOCK_ID, &ts) != 0)
+    {
+      perror ("clock_gettime");
+      abort ();
+    }
+  t = (double) ts.tv_sec + (double) ts.tv_nsec * 1.0e-9;
+  return t;
+}
+
+#else /* !_POSIX_TIMERS */
+#include <sys/time.h>
+
+double
+get_time (void)
+{
+  struct timeval tv;
+  double t;
+  if (gettimeofday (&tv, NULL) != 0)
+    {
+      perror ("gettimeofday");
+      abort ();
+    }
+  t = (double) tv.tv_sec + (double) tv.tv_usec * 1.0e-6;
+  return t;
+}
+
+#endif
